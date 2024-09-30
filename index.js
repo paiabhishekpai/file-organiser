577e16e211cd6be830e3c8b9bb598cbde9a81ab2
paiabhishekpai <abhishekupai1124@gmail.com>
2024-09-30 20:49:00 +0530

    creating seperate directories

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 import fs from "fs/promises"
+import fsn from "fs"
+import path from "path"
 
-let files = await fs.readdir("C:\\Users\\abhishek pai\\OneDrive\\Desktop\\projects\\file organiser")
-console.log(files);
+const basepath = "C:\\Users\\abhishek pai\\OneDrive\\Desktop\\projects\\file organiser"
+
+let files = await fs.readdir(basepath)
+
+for (const item of files) {
+    //getting the extensions
+    let ext=item.split(".")[item.split(".").length-1];
+    
+    //checking if directory exists or making directorires for each extension
+    if(fsn.existsSync(ext)){
+        //move files here
+    }
+    else{
+        //make a directory
+        fs.mkdir(ext);
+    }
+
+}
+
